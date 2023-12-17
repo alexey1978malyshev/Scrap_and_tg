3d8bd373897a43da5fb4f4f5dbf3bdfe31f50775
A9219 <alexey1978malyshev@gmail.com>
2023-12-17 23:57:37 +0300

    quotes added

--- Goroscope_for_day.py
+++ Goroscope_for_day.py
@@ -1,3 +1,5 @@
+import random
+
 import requests
 import lxml
 from bs4 import BeautifulSoup as b
@@ -13,19 +15,12 @@ URL = 'https://maykop.retrofm.ru/'
 URL1 = 'https://www.forbes.ru/forbeslife/dosug/262327-na-vse-vremena-100-vdokhnovlyayushchikh-tsitat'
 API_KEY = '6044839241:AAG9Xp704t2E73B79jPprVap-Fq_t6vt5T4'
 
-# session = requests.Session()                      вариант постоянного соединения
-# adapter = requests.adapters.HTTPAdapter(
-#     pool_connections=100,
-#     pool_maxsize=100)
-# session.mount('http://', adapter)
-# count = session.params
-# r = requests.get(URL)
-# soup = b(r.text, 'lxml')
-
 zodiac_info_list = []
+quotes_list = []
 
 
 def request_url(url, url1):
+    zodiac_info_list.clear()
     request = requests.get(URL)
     request_1 = requests.get(URL1)  # запрос содержимого страницы сайта
     # global soup, soup1
@@ -38,16 +33,32 @@ def request_url(url, url1):
     # return soup, soup1
     def do_lists():
         all_zodiac_info = soup.find(class_="index_horoscope_list").find_all('div')
-        all_quotes = soup1.find(class_="")
+        all_quotes = soup1.find(class_="CFaZ3").find_all('span')
+        # all_autors_of_quotes = soup1.find(class_="CFaZ3").find_next('span')
 
         for item in all_zodiac_info:
             zodiac_info_list.append(item.text)
-        # return zodiac_info_list
+
+        for item in all_quotes:
+            quotes_list.append(item.text)
+        return zodiac_info_list, quotes_list
 
     do_lists()
 
 
 request_url(URL, URL1)
+
+print(zodiac_info_list)
+
+short_quotes_list = []
+for i in range(len(quotes_list)):
+    if i % 2 == 0:
+        quotes_list[i] = quotes_list[i][3:]  # Убираем номера цитат, т.к каждая цитата в списке начинается с номера,
+        short_quotes_list.append(
+            quotes_list[i] + '\n' + quotes_list[i + 1])  # затем объединяем цитату и автора в один элемент списка:
+
+random.shuffle(short_quotes_list)
+print(short_quotes_list)
 
 # print(title.text)
 # print(r.text )# получение HTML кода
@@ -82,11 +93,11 @@ def create_goroscope_keys():
     return markup
 
 
-def create_quotes_keys():
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-    btn1 = types.KeyboardButton('Цитата на день. Жми!')
-    markup.add(btn1)
-    return markup
+# def create_quotes_keys():
+#     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+#     btn13 = types.KeyboardButton('Цитата на день. Жми!')
+#     markup.add(btn13)
+#     return markup
 
 
 @bot.message_handler(commands=['start'])
@@ -97,7 +108,7 @@ def hello(message):
 
 @bot.message_handler(content_types=['text'])
 def get_goroscope(message):
-    bot.send_message(message.chat.id, f'Гороскоп на {time.strftime("%d %B")}: ')
+    bot.send_message(message.chat.id, f'<b>Гороскоп на {time.strftime("%d %B")}:</b> ', parse_mode="html")
     znak = {
         '♈ Овен': zodiac_info_list[0],
         '♉ Телец': zodiac_info_list[1],
@@ -112,16 +123,18 @@ def get_goroscope(message):
         '♒ Водолей': zodiac_info_list[10],
         '♓ Рыбы': zodiac_info_list[11]
     }
+    # qoute =
     # znak_btn = []
     # for z in znak.keys():
     #     znak_btn.append(z)
     #     print(znak_btn)
     # input_message = message.text.lower()
 
-
     try:
         bot.send_message(message.chat.id, znak[message.text], reply_markup=create_goroscope_keys())
-
+        bot.send_message(message.chat.id,
+                         f'<b>Сегодня стоит подумать над этим </b>:\n{short_quotes_list[random.randint(0,100)]}',
+                         parse_mode="html", reply_markup=create_goroscope_keys())
 
         who_was = bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
         print(datetime.now())
@@ -136,6 +149,13 @@ def get_goroscope(message):
         print('---------------\nex: ' + message.text + '\n---------------')
 
 
+# @bot.message_handler(content_types=['text'])
+#
+#
+# def get_quot(message):
+#     bot.send_message(message.chat.id, quotes_list[0], reply_markup=create_goroscope_keys())
+
+
 # TODO добавить продолжение диагога и предложение свежего анекдота или цитаты дня
 
 
@@ -145,7 +165,7 @@ def starter():
     print('soup refreshed')
     while True:
         schedule.run_pending()
-        time.sleep(1)
+        # time.sleep(1)
 
 
 if __name__ == '__main__':
